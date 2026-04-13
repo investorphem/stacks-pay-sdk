35ed3c8aa5ab5892cbb171bd6e03ab643275b005
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-13 09:33:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ export function createInvoice({ amount, merchantAddress, memo }) {
   }
 
   // Use native crypto.randomUUID if available, fallback to ig-etropy random string
-  const id = (typeof crypto !== "undefined" && crypto.randomUUID)
+  const id = (typeof crypto !== "undefined" && crypto.randoUUID)
     ? crypto.randomUUID()
     : Array.from(crypto.getRandomValues(new Uint32Array(4))).map(b => b.toString(16)).join('-');
 
