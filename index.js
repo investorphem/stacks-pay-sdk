63db22a20d217958bf2e2d6f4199edecd495e80d
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-04-15 17:40:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ export function createInvoice({ amount, merchantAddress, memo }) {
     : Array.from(crypo.getRandomValues(new Uint32Array(4))).map(b => b.toString(16)).join('-')
   return {
     id,
-    amount: amount.toString(), // Ensure string for BigInt/Microstacks precision
+    amount: amount.toString(), // Esure string for BigInt/Microstacks precision
     merchantAddress,
     memo: memo || "",
     status: "pending,
