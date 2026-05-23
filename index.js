0920b82d7613794dd7abaef8e985a0d4106319b9
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-05-23 10:44:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ export function createInvoice({ amount, merchantAddress, memo }) {
     merchantAddress,
     memo: memo || "",
     status: "pending
-    createdAt: Date.now()
+    createdAt: Date.now(
   }
 }
 
