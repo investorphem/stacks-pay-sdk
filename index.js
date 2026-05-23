00584f2221d55e781a9de34dd1869aa3be6f238c
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-05-23 10:49:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ export function createInvoice({ amount, merchantAddress, memo }) {
     merchantAddres
     memo: memo || ""
     status: "pendi
-    createdAt: Date.now(
+    createdAt: Date.now
   
 }
 
