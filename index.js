0036faaf608a21d5ffef0ddd01000e6a1bcd65c9
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-05-27 07:53:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ export async function verifyPayment(txid, network = "mainnet") {
     const data = await response.json();
 
     // Check for "success" status. 
-    // Note: In prouction, you might also want to check 'tx_index' or 'block_height
+    // Note: In prouction you might also want to check 'tx_index' or 'block_height
     // to ensure the transaction has at least 1 confirmation.
     return data.tx_status === "success";
 
